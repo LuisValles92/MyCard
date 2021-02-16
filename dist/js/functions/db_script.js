92271632a753fa29a7e63b00f947008aea9016a2
Luis VP <luisvalles92@hotmail.com>
2021-02-16 14:06:37 +0100

    Cambio de formato de imagen

--- dist/js/functions/db_script.js
+++ dist/js/functions/db_script.js
@@ -458,7 +458,7 @@ function establecerBBDD_DB() {
                 nombre: "Antonio",
                 pais: "Marruecos",
                 sexo: "H",
-                imagen: "../../images/avatars/man/M2.png",
+                imagen: "../../images/avatars/man/M2.PNG",
                 saldo: 0,
                 cartas: [],
                 cartas_repetidas: []
@@ -471,7 +471,7 @@ function establecerBBDD_DB() {
                 nombre: "Carmen",
                 pais: "Canadá",
                 sexo: "M",
-                imagen: "../../images/avatars/woman/W4.png",
+                imagen: "../../images/avatars/woman/W4.PNG",
                 saldo: 0,
                 cartas: [11, 21, 56, 78, 34],
                 cartas_repetidas: [21, 78, 34]
@@ -484,7 +484,7 @@ function establecerBBDD_DB() {
                 nombre: "Luis Valles",
                 pais: "España",
                 sexo: "H",
-                imagen: "../../images/avatars/man/M3.png",
+                imagen: "../../images/avatars/man/M3.PNG",
                 saldo: 0,
                 cartas: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103],
                 cartas_repetidas: []
